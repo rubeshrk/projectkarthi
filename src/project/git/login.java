0390e162e5c865a994842167c06b62ed6eeaedd8
rubeshrk <rubesh29394@gmail.com>
2021-09-08 13:32:41 +0530

    conflict resolved

--- src/project/git/login.java
+++ src/project/git/login.java
@@ -2,23 +2,16 @@ package project.git;
 
 public class login {
 	
-<<<<<<< HEAD
+
 	private void passWord() {
 		System.out.println("pass");
-=======
+	}
 	private void userName() {
 		System.out.println("name");
-
-	}
-	public static void main(String[] args) {
-		login li = new login();
-		li.userName();	
-	}
->>>>>>> 952480ef3736b24e92ad9b5050a4a9af172c934a
-
 	}
 	public static void main(String[] args) {
 		login li = new login();
 		li.passWord();
+		li.userName();
 	}
 }
