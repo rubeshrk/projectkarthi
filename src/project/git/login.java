955bc625a1f7b47b0d4f666136893b77234518c5
rubeshrk <rubesh29394@gmail.com>
2021-09-08 10:16:31 +0530

    password functionality

--- src/project/git/login.java
+++ src/project/git/login.java
@@ -1,5 +1,13 @@
 package project.git;
 
 public class login {
+	
+	private void passWord() {
+		System.out.println("pass");
 
+	}
+	public static void main(String[] args) {
+		login li = new login();
+		li.passWord();
+	}
 }
